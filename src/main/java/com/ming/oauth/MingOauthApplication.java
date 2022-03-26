85a749f6ae0b0a5c505f5998c53cb34f28d7ffbd
Minjae Kim <minjae.kim@mytrum.com>
2022-03-26 10:40:53 +0900

    Initial commit

---
+++ src/main/java/com/ming/oauth/MingOauthApplication.java
@@ -0,0 +1,14 @@
+package com.ming.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingOauthApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MingOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ming/oauth/MingOauthApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ming.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
