69b9d0cb8bfebfcbbbb47826a2de0e420e875c4a
Minjae Kim <minjae.kim@mytrum.com>
2022-03-27 21:41:30 +0900

    index 페이지 생성 및 인증 페이지 configuration 생성 -{3}

--- src/main/java/com/ming/oauth/MingOauthApplication.java
+++ src/main/java/com/ming/oauth/MingOauthApplication.java
@@ -2,13 +2,30 @@ package com.ming.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @SpringBootApplication
-public class MingOauthApplication {
+public class MingOauthApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
 
         SpringApplication.run(MingOauthApplication.class, args);
     }
 
+    // security filter
+    //permitted endpoints에 대한 화이트 리스트 설정
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(a -> a.antMatchers("/", "/error", "/webjars/**")
+                .permitAll()
+                .anyRequest().authenticated())
+                .exceptionHandling(e -> e.authenticationEntryPoint(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                ))
+                .oauth2Login();
+    }
 }
